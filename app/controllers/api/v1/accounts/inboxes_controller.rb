b246db26fa71690f2657288cf47675dc6932a48b
Alicia Codex <alicia@schlaile.de>
2026-09-13 18:41:47 +0200

    style: keep inbox controller within lint limit

--- app/controllers/api/v1/accounts/inboxes_controller.rb
+++ app/controllers/api/v1/accounts/inboxes_controller.rb
@@ -1,11 +1,11 @@
 class Api::V1::Accounts::InboxesController < Api::V1::Accounts::BaseController
   include Api::V1::InboxesHelper
+  include Api::V1::Accounts::Concerns::WhatsappHealthManagement
+
   before_action :fetch_inbox, except: [:index, :create]
   before_action :fetch_agent_bot, only: [:set_agent_bot]
   # we are already handling the authorization in fetch inbox
   before_action :check_authorization, except: [:show]
-
-  include Api::V1::Accounts::Concerns::WhatsappHealthManagement
 
   def index
     @inboxes = policy_scope(Current.account.inboxes)
